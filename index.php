9be2a057c14bc749ca125948baa31aea22fc250f
Andy at Spanner Spotter <andy@spannerspotter.com>
2012-04-29 21:09:04 +0100

    Change home page to 2012

--- index.php
+++ index.php
@@ -35,7 +35,7 @@ $path = GalleryUrlGenerator::getCurrentRequestUri();
 
 if ($path == '/') {
   /* Alter front page to current year's album */
-  $path = '/v/2011/';
+  $path = '/v/2012/';
 }
 elseif (preg_match('|^(/(?:[^?#/]+/)*)(.*)|', $path, $matches)) {
   $path = $matches[1] . GALLERY_MAIN_PHP;
